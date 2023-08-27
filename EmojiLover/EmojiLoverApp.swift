a62afab590708fe075380fd8605e9714275795c4
Jean Silvany <jeansilvany@hotmail.com>
2023-08-27 19:14:13 -0300

    first commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Jean Silvany on 27/08/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
